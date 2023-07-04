553601dc29fc64375b3f342dd250cbf51ce2ca4d
Roxana Maria Rasteiu <maria_roxy11@yahoo.com>
2023-07-04 18:52:43 +0300

    New commit

--- tests.py
+++ tests.py
@@ -6,18 +6,18 @@ diff = calculateSum.diffV
 multiplication = calculateSum.multiplicationV
 division = calculateSum.divisionV
 
-# 1st test
+# 1st test:
 assert add == 18 , Fore.RED + "1st TEST --> FAILED"
 print(Fore.GREEN + "1st TEST --> PASSED")
 
-# 2nd test
+# 2nd test:
 assert diff == 12 , Fore.RED + "2nd TEST --> FAILED"
 print(Fore.GREEN + "2nd TEST --> PASSED")
 
-# 3rd test
+# 3rd test:
 assert multiplication == 45 , Fore.RED + "3rd TEST --> FAILED"
 print(Fore.GREEN + "3rd TEST --> PASSED")
 
-# 4th test
+# 4th test:
 assert division == 5 , Fore.RED + "4th TEST --> FAILED"
 print(Fore.GREEN + "4th TEST --> PASSED")
